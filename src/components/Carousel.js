f16784b14192d65242cc7028173145064306e9c0
Bytee-bit <deepak2gupta99@gmail.com>
2023-02-06 12:08:41 +0530

    extra div added in carosual marker display

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -16,48 +16,56 @@ export function Carousel({ children: slides, autoSlide, duration }) {
   return (
     <Box
       sx={{
-        overflow: "hidden",
-        border: "1px solid #000",
+        // border: "1px solid #000",
         position: "relative",
       }}
     >
       <Box
-        // className="slide"
         sx={{
-          display: "flex",
-          transform: `translateX(-${curr * 100}%)`,
-          transition: "all 500ms ease-out",
+          overflow: "hidden",
+          // border: "1px solid #000",
+          // position: "relative",
+          borderRadius: "4px",
         }}
       >
-        {slides}
-      </Box>
-      {/* <Box sx={{ position: "absolute", top: "0px", display: "flex" }}>
+        <Box
+          // className="slide"
+          sx={{
+            display: "flex",
+            transform: `translateX(-${curr * 100}%)`,
+            transition: "all 500ms ease-out",
+          }}
+        >
+          {slides}
+        </Box>
+        {/* <Box sx={{ position: "absolute", top: "0px", display: "flex" }}>
         <Typography onClick={prev}>prev</Typography>
         <Typography onClick={next}>next</Typography>
       </Box> */}
-      <Box
-        sx={{
-          position: "absolute",
-          bottom: "0px",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          width: "100%",
-          gap: "8px",
-        }}
-      >
-        {slides.map((item, idx) => (
-          <Box
-            sx={{
-              width: "16px",
-              height: "16px",
-              borderRadius: "50%",
-              background: "red",
-              p: curr === idx ? "2px" : "0px",
-              opacity: curr === idx ? "1" : "0.5",
-            }}
-          ></Box>
-        ))}
+        <Box
+          sx={{
+            position: "absolute",
+            bottom: "-48px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "flex-end",
+            width: "100%",
+            gap: "8px",
+          }}
+        >
+          {slides.map((item, idx) => (
+            <Box
+              sx={{
+                width: "24px",
+                height: "24px",
+                borderRadius: "50%",
+                background: "#C1CFFF",
+                p: curr === idx ? "2px" : "0px",
+                opacity: curr === idx ? "1" : "0.5",
+              }}
+            ></Box>
+          ))}
+        </Box>
       </Box>
     </Box>
   );
